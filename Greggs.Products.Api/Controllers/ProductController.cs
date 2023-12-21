0d288906d8b29f5d6250c75b9aa0be62bc10f11a
Ethan Wilson <E.Wilson17@newcastle.ac.uk>
2023-12-21 16:50:47 +0000

    Added list of potential improvements

--- Greggs.Products.Api/Controllers/ProductController.cs
+++ Greggs.Products.Api/Controllers/ProductController.cs
@@ -53,6 +53,7 @@ public class ProductController : ControllerBase
 
         1. Product price in pounds no longer applies when converting to Euro, could map the object to another with price in Euros.
         2. Sort by name or sort by price
+        3. Search by name for specific product
 
         */
 
